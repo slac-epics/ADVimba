fa40604497f8ae017d2e9f130455d7873c9e0ea8
Michael Browne <mcbrowne@slac.stanford.edu>
2021-10-28 16:16:11 -0700

    Using new binaries from upstream.

---
+++ vimbaApp/reset_src/reset_usb.c
@@ -0,0 +1,40 @@
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<libusb.h>
+
+static int reset_usb(char *serial)
+{
+    int n = 0;
+    libusb_context* pctx;
+    libusb_device **list;
+    unsigned char buf[1024];
+    int i;
+
+    libusb_init(&pctx);
+    n = libusb_get_device_list(pctx, &list);
+    for (i = 0; i < n; i++) {
+        struct libusb_device_descriptor desc;
+	libusb_device_handle *h;
+        libusb_get_device_descriptor(list[i], &desc);
+	if (libusb_open(list[i], &h))
+	    continue;
+	libusb_get_string_descriptor_ascii(h, desc.iSerialNumber, buf, sizeof(buf));
+	if (!strcmp((char*)buf, serial)) {
+	    printf("Found %s, resetting.\n", serial);
+	    libusb_reset_device(h);
+	    libusb_close(h);
+	    break;
+	}
+	libusb_close(h);
+    }
+    libusb_exit(pctx);
+    return n;
+}
+
+int main(int argc, char **argv)
+{
+    reset_usb(argv[1]);
+    return 0;
+}
